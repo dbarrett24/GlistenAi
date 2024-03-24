0bc918f97dc395e859e22fd97502b9413a7ee8d0
Dave Barrett <dev.davebarrett@gmail.com>
2024-03-23 21:51:58 -0600

    Initial Commit of Dark Glassmorphic Prismic Nextjs landing page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import WordMark from "@/components/WordMark";
+import { createClient } from "@/prismicio";
+import { PrismicNextLink } from "@prismicio/next";
+import Link from "next/link";
+
+const Footer = async () => {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+
+  return (
+    <footer className="flex flex-col items-center justify-between gap-6 border-t border-slate-600 px-8 py-7 md:flex-row"></footer>
+  );
+};
+
+export default Footer;
